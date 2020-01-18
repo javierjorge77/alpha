8e4679be830f852add7442ee30ad856643a0e22c
ubuntu <javierjorge77@gmail.com>
2020-01-18 05:59:10 +0000

    first create of flash error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,11 +8,22 @@ class ArticlesController < ApplicationController
   def create
     #render plain: params[:article].inspect
     @article= Article.new (article_params)
-    @article.save
-    redirect_to articles_show(@article)
-    
+  
+    if @article.save
+      flash[:notice]= "Article sucessfuly saved"
+      redirect_to article_path(@article)
+    else 
+      render "new"
+    end
     
   end
+  
+  def show
+    @article= Article.find(params[:id])
+    
+  end 
+  
+  
   
   private
   def article_params
