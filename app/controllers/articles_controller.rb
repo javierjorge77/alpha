fca5171ed4d5248c096eba7d219d96226bbbd83d
ubuntu <javierjorge77@gmail.com>
2020-02-02 03:23:08 +0000

    partials & Destroy Action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,6 +28,15 @@ class ArticlesController < ApplicationController
     
   end 
   
+  def destroy
+    @article= Article.find(params[:id])
+    @article.destroy
+    flash[:notice]= "Article was sucessfully deleted"
+    redirect_to articles_path
+    
+  end
+  
+  
   def edit
   @article = Article.find (params[:id])
   end
